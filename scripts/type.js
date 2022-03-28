6eed81e30acad23427d221a2f49042de44986585
vanGit1 <97252986+vanGit1@users.noreply.github.com>
2022-03-28 14:12:50 +0300

    add type script

---
+++ scripts/type.js
@@ -0,0 +1,5 @@
+$('#typing').typeIt({
+	strings: 'The starting point of all achievement is desire...',
+	speed: 100, //скорость анимации
+	autoStart: false /*если true то анимация начнется сразу после загрузки страницы, если false, то только когда блок будет в зоне видимости*/,
+});
