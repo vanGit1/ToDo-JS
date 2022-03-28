2b7d9646e7972dfc8a0c60136e65f2a5bbae14f1
vanGit1 <97252986+vanGit1@users.noreply.github.com>
2022-03-28 12:55:44 +0300

    Update toggle.js

--- scripts/toggle.js
+++ scripts/toggle.js
@@ -1,15 +1,15 @@
 let checkbox = document.querySelector('.theme-checkbox');
 
 if (localStorage.getItem('theme') == 'true') {
-	theme.setAttribute('href', 'css/dark.css');
+	theme.setAttribute('href', 'CSS/dark.css');
 	checkbox.checked = true;
 }
 checkbox.onchange = function () {
 	if (this.checked) {
 		localStorage.setItem('theme', true);
-		theme.setAttribute('href', 'css/dark.css');
+		theme.setAttribute('href', 'CSS/dark.css');
 	} else {
 		localStorage.setItem('theme', false);
-		theme.setAttribute('href', 'css/light.css');
+		theme.setAttribute('href', 'CSS/light.css');
 	}
 };
